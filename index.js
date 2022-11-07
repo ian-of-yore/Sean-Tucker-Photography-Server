828a722524196d3036a15a7df79bed0d5ff5d9df
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-07 22:50:49 +0600

    mongoDB read operation for homepage and services page done

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const data = require('./data/data.json');
 
-app.get('/', (req, res) => {
-    res.send("Server running")
-})
 
-app.get('/services', (req, res) => {
-    res.send(data);
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mmmt3qa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db("SeanTucker");
+        const servicesCollection = database.collection("services");
+
+        // sending only 3 services data to the client
+        app.get('/', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // sending all services to the client
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch((err) => console.log(err))
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
