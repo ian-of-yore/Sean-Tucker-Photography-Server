47960210357ab905e3784215353adefafb4ee8fe
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-09 14:39:24 +0600

    delete user review from database done

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
             res.send(userReviews);
         })
 
+        // deleting user provided review from the database
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
     }
